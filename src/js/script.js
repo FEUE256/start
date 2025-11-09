ab8c1f64e806967a233553afd13c7b5b70e2cf79
FÈUE <feue.com1@outlook.com>
2025-11-09 18:15:08 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+ document.querySelectorAll('ul li').forEach(li => {
+        li.addEventListener('click', () => {
+            alert(li.textContent);
+        });
+    });
